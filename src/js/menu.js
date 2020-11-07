038af25886c5430761adb1bbb820a4ffbdf472f8
zhukouskayat <64001084+zhukouskayat@users.noreply.github.com>
2020-11-07 11:59:25 +0300

    Updated menu function

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,7 +1,11 @@
+/* eslint-disable no-param-reassign */
+/* eslint-disable import/prefer-default-export */
 /* eslint-disable func-names */
 
-document.querySelector('.menu-icon').onclick = function(){
-  const menuElem = document.getElementById('menu-items');
-  this.classList.toggle('open');
-  menuElem.classList.toggle('active');
+export function menu (doc) {
+  doc.querySelector('.menu-icon').onclick = function(){
+    const menuElem = document.getElementById('menu-items');
+    this.classList.toggle('open');
+    menuElem.classList.toggle('active');
+  }
 }
